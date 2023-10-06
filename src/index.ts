000c73cd9ecd5846e3ada10e6304d1d8f683c212
Evan Hsu <evanhsu@gmail.com>
2023-10-05 20:05:06 -0600

    Updated doc blocks and error messages to include .env file path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { config, DotenvConfigOptions } from 'dotenv';
-import { AnyZodObject, z } from 'zod';
+import { config, DotenvConfigOptions } from "dotenv";
+import { AnyZodObject, z } from "zod";
 
 /**
+ * Loads values from the .env file and validates them using the provided Zod schema.
+ * Only values defined in the Zod schema will be returned from this function, even
+ * if the .env file includes additional values.
  *
  * @throws Error
  */
@@ -9,8 +12,10 @@ function loadEnv<T extends AnyZodObject = AnyZodObject>(
   envSchema: T,
   loadOptions?: DotenvConfigOptions
 ) {
+  // Read the entire env file.  Any keys that aren't included in the provided 'envSchema' will
+  // be stripped out of the return value from this function below where we call Zod.parse()
   const rawEnv = config(loadOptions ?? {});
-  if (typeof rawEnv.error !== 'undefined') {
+  if (typeof rawEnv.error !== "undefined") {
     throw rawEnv.error;
   }
 
@@ -18,8 +23,9 @@ function loadEnv<T extends AnyZodObject = AnyZodObject>(
   try {
     parsedEnv = envSchema.parse({ ...rawEnv, ...process.env });
   } catch (error /* ZodError */) {
+    const envPath = loadOptions?.path ?? ".env";
     throw new Error(
-      `Invalid configuration loaded. Check these values in your .env file: ${error}`
+      `Invalid configuration loaded. Check these values in your ${envPath} file: ${error}`
     );
   }
 
